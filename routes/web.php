b4bbca19041d2f26617920420b4711357393767f
alipileforooshha <alipileforooshha@gmail.com>
2023-01-26 15:36:23 +0330

    add landing page

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\ItemsController;
 use App\Http\Controllers\SalesController;
 use Illuminate\Support\Facades\Route;
 
+Route::view('/','landing')->name('landing');
 Route::view('/login','login')->name('login');
 Route::view('/register','register')->name('register');
 Route::post('/login',[AuthController::class, 'login'])->name('login.post');
